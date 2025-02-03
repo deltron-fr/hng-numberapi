092fecbeea3588aec2d84051a38b1acee832c368
deltron-fr <kayceeogbonnaya2304@gmail.com>
2025-02-03 15:24:32 +0100

    Mades changes to the views.py

--- numsapi/numsapiapp/views.py
+++ numsapi/numsapiapp/views.py
@@ -14,50 +14,55 @@ def is_prime(number):
         return True
     
 def is_perfect(number):
+    if number <= 0:
+        return False
     divisors_sum = sum([i for i in range(1, number) if number % i == 0])
     return divisors_sum == number
 
 def get_properties(number):
     power = len(str(number))
     sum_of_digits = 0
-
-    for i in str(number):
-        sum_of_digits += int(i)**power
-    if sum_of_digits == number and (number % 2 == 0):
-        return ["armstrong", "even"]
-    elif sum_of_digits == number and (number % 2 != 0):
-        return ["armstrong", "odd"]
-    elif sum_of_digits != number and (number % 2 == 0):
-        return ["even"]
+    if number >= 0:
+        for i in str(number):
+            if i != ".":
+                sum_of_digits += int(i)**power
+        if sum_of_digits == number and (number % 2 == 0):
+            return ["armstrong", "even"]
+        elif sum_of_digits == number and (number % 2 != 0):
+            return ["armstrong", "odd"]
+        elif sum_of_digits != number and (number % 2 == 0):
+            return ["even"]
+        else:
+            return ["odd"]
     else:
-        return ["odd"]
+        return ["even"] if number % 2 == 0 else ["odd"]
 
 def digit_sum(number):
-    return sum([int(i) for i in str(number)])
+    return sum(int(digit) for digit in str(abs(int(number))))
 
 def get_fun_fact(number):
-    api_url = f"http://numbersapi.com/{number}/math"
+    api_url = f"http://numbersapi.com/{int(number)}/math"
     try:
         response = requests.get(api_url, timeout=5)
         response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:  
-        return response.get('text', f"No fun fact available for {number}.")
+        return f"No fun fact available for {number}."
 
 
 @api_view(['GET'])
 def classify_number(request):
     number_str = request.GET.get("number")
     
-    if not str(number_str).isdigit():
+    try:
+        number = float(number_str)
+    except ValueError:
         return Response({"number":number_str, "error": True}, status=status.HTTP_400_BAD_REQUEST)
-    
-    number = int(number_str)
         
-    prime = is_prime(number)
-    perfect = is_perfect(number)
+    prime = is_prime(int(number)) 
+    perfect = is_perfect(int(number)) 
     properties = get_properties(number)
-    sum_of_digits = digit_sum(number)
+    sum_of_digits = digit_sum(int(abs(number)))
     fun_fact = get_fun_fact(number)
 
     data = {
@@ -69,8 +74,10 @@ def classify_number(request):
         "fun_fact": fun_fact
     }
 
-    serializer = NumberSerializer(data)
-    return Response(serializer.data)
+    serializer = NumberSerializer(data=data)
+    if serializer.is_valid():
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
